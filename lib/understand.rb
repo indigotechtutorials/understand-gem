c0d39c9ad8cf5af3203f2aaa308d5df9e54e2130
Enoch Tamulonis <enochtamulonis@gmail.com>
2026-02-10 13:58:36 +0700

    MVP understand tech

--- lib/understand.rb
+++ lib/understand.rb
@@ -1,7 +1,21 @@
 require "rllama"
 
 class Understand
-  def self.hello
-    puts 'Hello world'
+  attr_reader :user_prompt
+  def initialize(user_prompt)
+    @user_prompt = user_prompt
+  end
+
+  def call
+    model = Rllama.load_model('lmstudio-community/gemma-3-1B-it-QAT-GGUF/gemma-3-1B-it-QAT-Q4_0.gguf')
+    files = Dir.glob("**/*").select { |f| File.file?(f) }
+    files.each do |file|
+      prompt_for_ai = user_prompt + "--- File path: #{file} Content: #{File.read(file)}"
+      puts "💡 🔎 Understanding #{file}"
+      result = model.generate(prompt_for_ai) do |token|
+        print token
+      end
+    end
+    model.close
   end
 end
